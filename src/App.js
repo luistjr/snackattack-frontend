37db561e239edf7a21d763c6b900a23af22e6082
BMoClay <benmorgancleveland@gmail.com>
2021-02-10 18:48:36 -0500

    setup front end to connect to backend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React, { useEffect } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    fetch("http://[::1]:3001/snacks")
+    .then(r => r.json())
+    .then(snacksArray => {
+      return console.log(snacksArray)
+    })
+  }, []) 
+
   return (
     <div className="App">
       <header className="App-header">
