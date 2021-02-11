e1fc71db339f548f03ae4ecb00ba8bd6b6b310fd
therunningagent <luistjr@gmail.com>
2021-02-11 14:06:34 -0700

    old update

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import './App.css';
+import '../App.css';
 import Header from './Header';
 import Navbar from './NavBar';
 import SnackContainer from './SnackContainer';
