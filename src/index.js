8fb2e619f1a07d0d2f76d6c11c321ee033663d4c
BMoClay <benmorgancleveland@gmail.com>
2021-02-11 16:44:15 -0500

    navbar start, react router install

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router} from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+    <Router>
+      <App />
+    </Router>,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
-reportWebVitals();
+// reportWebVitals();
