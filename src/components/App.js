167c27ab529a53ecee4d56e12e1a732fa04681c2
BMoClay <benmorgancleveland@gmail.com>
2021-02-11 17:30:11 -0500

    Add profile

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, { useEffect } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import '../App.css';
 import Header from './Header';
 import Navbar from './NavBar';
 import SnackContainer from './SnackContainer';
-
+import LoginForm from './LoginForm';
+import SignUpForm from './SignUpForm';
+import Profile from './Profile';
 
 function App() {
 
@@ -16,8 +19,19 @@ function App() {
   // }, []) 
 
   return (
-    <div>
+    <div className="App">
         <Navbar />
+        <Switch>
+          <Route path="/Login">
+            <LoginForm />
+          </Route>
+          <Route path="/New">
+            <SignUpForm />
+          </Route>
+          <Route path="/Profile">
+            <Profile />
+          </Route>
+        </Switch>
         <Header />
         <SnackContainer />
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,10 +7,10 @@ function NavBar() {
 
   return (
       <nav class="NavBar">
-          <Link to="/" exact className="button">Home</Link>
+          <Link to="/Profile" exact className="button">Profile</Link>
           <Link to="/Login" exact className="button">Login</Link>
           <Link to="/Logout" exact className="button">Logout</Link>
-          <Link to="/Signup" exact className="button">Signup</Link>
+          <Link to="/New" exact className="button">Signup</Link>
           <Link to="/Stash" exact className="button">Stash</Link>
       </nav>
   );
