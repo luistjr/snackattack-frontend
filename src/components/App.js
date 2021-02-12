6b7a23d8261670968fb9cbcbeb4ff1b52fda688f
BMoClay <benmorgancleveland@gmail.com>
2021-02-12 09:22:08 -0500

    profile component first build

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import '../App.css';
 import Header from './Header';
@@ -9,6 +9,7 @@ import SignUpForm from './SignUpForm';
 import Profile from './Profile';
 
 function App() {
+  const [user, setUser] = useState([])
 
   // useEffect(() => {
   //   fetch("http://[::1]:3001/snacks")
@@ -18,6 +19,22 @@ function App() {
   //   })
   // }, []) 
 
+  useEffect(() => {
+    fetch("http://[::1]:3001/users")
+    .then(r => r.json())
+    .then(usersArray => {
+        setUser(usersArray.map(user => {
+          return <Profile
+          key={user.id}
+          user={user}
+          name={user.name}
+          age={user.age}
+          lifestyle={user.lifestyle}/>
+      }))
+    })
+  }, []) 
+
+  console.log(user)
   return (
     <div className="App">
         <Navbar />
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
-function Profile() {
+function Profile({ user }) {
+// function Profile() {
+    // const { name, age, lifestyle } = user
+    // const { id, name, age, lifestyle } = user
+    console.log(user)
 
     return (
        <div>
            <h1>Profile</h1>
+           <ul>
+               <li className="profile-details">
+                   {/* <h4>{name}</h4>
+                   <h4>{age}</h4>
+                   <h4>{lifestyle}</h4> */}
+                   <button>Edit Profile</button>
+                   <button>Delete Account</button>
+               </li>
+           </ul>
        </div>
     )
 }
