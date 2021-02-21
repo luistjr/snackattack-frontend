1ea4bd79622a97d302eba4a21d777282fecf1a7b
therunningagent <luistjr@gmail.com>
2021-02-21 16:18:57 -0700

    updated request

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,8 @@ function Header() {
 
   const img_url = `${process.env.REACT_APP_RAILS_URL}/images/header_logo.gif`
 
+  console.log(img_url);
+
   return (
       <div>
        <img className="header-image" src={img_url} />
